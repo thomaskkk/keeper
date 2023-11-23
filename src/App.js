ef6d00c7e435e4e7f4800cfd9173a356ccbbce1d
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-23 15:42:19 -0300

    Adding create notes and note

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
+import CreateArea from "./components/CreateArea";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="App">
       <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
