06d1a53bc5cfd2a3e6bfafcd4a9c58d31640c275
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-23 15:29:57 -0300

    Adding header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
+      <Footer />
     </div>
   );
 }
