bc50f8208d0e5c05dd66b5667e962f669903dd89
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-23 21:15:37 -0300

    Added roboto font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,5 @@
 import "./Header.css";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 function Header() {
   return (
